90dfa01baa0bbd1722fc6c9b8581909c64241ebc
davzshaw <juandis0246@gmail.com>
2025-05-26 14:21:19 -0500

    Riffusion working!

--- src/riffusion/riffusion.service.ts
+++ src/riffusion/riffusion.service.ts
@@ -21,31 +21,24 @@ export class RiffusionService {
       num_inference_steps = 50,
     } = body;
 
-    const TIMEOUT = 1000 * 60 * 10;
+    const modelUrl = 'riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05';
 
-    const generate = this.replicate.run(
-      'riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05',
-      {
-        input: {
-          prompt_a: promptA,
-          prompt_b: promptB,
-          alpha: alpha,
-          denoising: denoising,
-          seed_image_id: seed_image_id,
-          num_inference_steps: num_inference_steps,
-        },
-      },
-    );
-
-    const timeout = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Timeout reached')), TIMEOUT),
-    );
-
-    const output = await Promise.race([generate, timeout]);
-
-    return {
-      riffusionResponse: output,
+    const input = {
+      prompt_a: promptA,
+      prompt_b: promptB,
+      alpha,
+      denoising,
+      seed_image_id,
+      num_inference_steps,
     };
-  }
 
+    const prediction = await this.replicate.predictions.create({
+      version: modelUrl,
+      input,
+      wait: true,
+    });
+    return prediction;
+
+
+  }
 }
